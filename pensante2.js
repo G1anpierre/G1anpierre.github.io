c7a694d0882285d99bef320944287c68a028fb02
Gianpierre Fernandez <Gianpi@Gianpierre-MacBook-Pro.local>
2019-09-24 17:07:13 +0200

    add better structur js and change html title

--- pensante2.js
+++ pensante2.js
@@ -1,8 +1,37 @@
-let randomNumber1 = Math.floor(Math.random()*6) + 1;
-let randomNumber2 = Math.floor(Math.random()*6) + 1;
+// Selectors
 
 const firstDice = document.querySelector(".img1");
 const secondDice = document.querySelector(".img2");
 
-firstDice.setAttribute("src", `images/dice${randomNumber1}.png`);
-secondDice.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+// windows open
+
+comando1();
+comando2();
+
+
+// Functions 
+
+function comando1(){
+    randomNumber1 = Math.floor(Math.random()*6) + 1;
+    firstDice.setAttribute("src", `images/dice${randomNumber1}.png`);
+}
+
+function comando2(){
+    let randomNumber2 = Math.floor(Math.random()*6) + 1;
+    secondDice.setAttribute("src", `images/dice${randomNumber2}.png`);
+}
+
+
+// click Dices 
+
+
+firstDice.addEventListener("click", function(){
+    console.log('img 1 was click');
+    comando1();
+});
+
+secondDice.addEventListener("click", function(){
+    console.log('img 2 was clisk');
+    comando2();
+});
