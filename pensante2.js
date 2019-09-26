af957ce0b69c114ae580f9b9ec04422042980b00
Gianpierre Fernandez <Gianpi@Gianpierre-MacBook-Pro.local>
2019-09-26 20:22:15 +0200

    change code to the random user generator project

--- pensante2.js
+++ pensante2.js
@@ -1,37 +1,52 @@
 // Selectors
 
-const firstDice = document.querySelector(".img1");
-const secondDice = document.querySelector(".img2");
 
+const imagen = document.querySelector('img');
+const name = document.querySelector('#name');
+const lastName = document.querySelector('#lastName');
+const other = document.querySelector('#otherInfo');
+const email = document.querySelector('#email');
+const city = document.querySelector('#city');
 
-// windows open
+const but = document.querySelector('button');
+var url = 'https://randomuser.me/api/'
 
-comando1();
-comando2();
+fetchCall();
 
-
-// Functions 
-
-function comando1(){
-    randomNumber1 = Math.floor(Math.random()*6) + 1;
-    firstDice.setAttribute("src", `images/dice${randomNumber1}.png`);
-}
-
-function comando2(){
-    let randomNumber2 = Math.floor(Math.random()*6) + 1;
-    secondDice.setAttribute("src", `images/dice${randomNumber2}.png`);
-}
-
-
-// click Dices 
-
-
-firstDice.addEventListener("click", function(){
-    console.log('img 1 was click');
-    comando1();
+but.addEventListener('click', function(){
+    fetchCall();
 });
 
-secondDice.addEventListener("click", function(){
-    console.log('img 2 was clisk');
-    comando2();
-});
+function fetchCall(){
+    fetch(url)
+    .then(handleErrors)
+    .then(parseJSON)
+    .then(updateProfile)
+    .catch(alertError);
+
+    function handleErrors(request){
+        if(request.status !== 200){
+            throw Error('A SUPER FATAL ERROR :)');
+        }
+        return request;
+    }
+
+    function parseJSON(response){
+        let data = response.json();
+        return data;
+    }
+
+    function updateProfile(dataReady){
+        imagen.setAttribute("src", dataReady.results[0].picture.medium);
+        name.textContent = dataReady.results[0].name.first;
+        lastName.textContent = dataReady.results[0].name.last;
+        other.textContent = dataReady.results[0].login.username;
+        email.textContent = dataReady.results[0].email;
+        city.textContent = dataReady.results[0].location.city;
+    }
+
+    function alertError(responseError){
+        alert('An error has occur ' + responseError);
+    }
+
+}
